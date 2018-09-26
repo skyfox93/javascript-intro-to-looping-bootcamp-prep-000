b11b7c308c4cdd133cd0d48152538a62c359fe84
Skylar Salerno <skylar.salerno@gmail.com>
2018-09-26 02:50:23 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,5 @@
 function forLoop(items){for(var i=0;i<25;i++){if(i==1){items.push("I am one strange loop.");} else{items.push(`I am ${i} strange loops.`);}}}
 
 function whileLoop(n){while(n>0){console.log(n);n--;if(n===0){console.log('Done');}}}
+
+function doWhileLoop(list)
