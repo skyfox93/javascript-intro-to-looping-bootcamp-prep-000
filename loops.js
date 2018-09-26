1d3ba49285079a62fe7d485c82420bf067007f05
Skylar Salerno <skylar.salerno@gmail.com>
2018-09-26 02:45:26 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,1 +1,1 @@
-function forLoop(items){for(var i=0;i<25;i++){if(i=1){items.push("I am one strange loop.");} else{items.push(`I am ${i} strange loops.`):}}}
+function forLoop(items){for(var i=0;i<25;i++){if(i==1){items.push("I am one strange loop.");} else{items.push(`I am ${i} strange loops.`):}}}
